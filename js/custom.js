6a8d366722f4afaccdfcc3aafe9708536a256658
Nadimh2218 <nadimh2218@gmail.com>
2024-03-30 01:09:07 +0600

    mad graphics

---
+++ js/custom.js
@@ -0,0 +1,38 @@
+
+
+
+
+
+$(function(){
+
+
+    $(window).on("load",function(){
+      $("#preloader").delay(200).fadeOut(500)
+    })
+    
+    
+  
+    
+    
+   
+    
+   
+    
+    
+        $('.banner_main').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed:2000,
+        arrows:false,
+        dots:true,
+        autoplay: true
+      });
+  
+       
+    
+
+    
+    })
+       
+ 
